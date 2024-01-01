d8638929030825c11acff09103e592db2837d4c0
Thor Whalen <thorwhalen1@gmail.com>
2024-01-01 10:47:45 +0000

    feat: arxiv

---
+++ imbed/mdat/arxiv.py
@@ -0,0 +1,108 @@
+"""Access to ArXiv data.
+
+The data was found at https://alex.macrocosm.so/download
+
+Direct download 
+"""
+
+from typing import Mapping, Literal, Iterable, KT, Union
+from functools import partial, lru_cache
+import io
+from itertools import chain
+from dol import (
+    FilesOfZip,
+    remove_mac_junk_from_zip,
+    add_ipython_key_completions,
+    Pipe,
+    cached_keys,
+    wrap_kvs,
+    KeyCodecs,
+    FuncReader,
+    KeyTemplate,
+)
+from dol.kv_codecs import common_prefix_keys_wrap
+import pandas as pd
+from imbed.util import MappingFactory, grazed_path
+
+# from tabled import ColumnOrientedMapping
+
+_raw_store = Pipe(
+    grazed_path,
+    FilesOfZip,
+    remove_mac_junk_from_zip,
+    common_prefix_keys_wrap,
+    add_ipython_key_completions,
+)
+
+urls = {
+    'titles': 'https://drive.google.com/file/d/1Ul5mPePtoPKHZkH5Rm6dWKAO11dG98GN/view?usp=share_link',
+    'abstracts': 'https://drive.google.com/file/d/1g3K-wlixFxklTSUQNZKpEgN4WNTFTPIZ/view?usp=share_link',
+}
+raw_sources = FuncReader({name: partial(_raw_store, url) for name, url in urls.items()})
+
+# --------------------------------------------------------------------------------------
+# The data-specific part
+# TODO: Ugly and unclean. Maybe just do with wrap_kvs and lambdas?
+
+import pandas as pd
+
+_key_template_kwargs = dict(
+    field_patterns=dict(
+        kind='\w+', number=r'\d+'
+    ),  # the pattern to match is an integer
+    from_str_funcs=dict(number=int),  # transform integer string into actual integer
+)
+_parquet_codec_end_pipe = (
+    wrap_kvs(obj_of_data=Pipe(io.BytesIO, pd.read_parquet)),  # get a dataframe
+    cached_keys(keys_cache=sorted),  # sort the keys when iterating
+)
+
+titles_key_template = KeyTemplate(
+    'titles_{number:d}.parquet',  # this is the template/pattern for the keys
+    **_key_template_kwargs,
+)
+titles_parquet_codec = Pipe(
+    titles_key_template.filt_iter('str'),  # filter in only keys that match the pattern
+    titles_key_template.key_codec('single'),  # just get a single integer as the key
+    *_parquet_codec_end_pipe,
+)
+abstracts_key_template = KeyTemplate(
+    'abstracts_{number:d}.parquet',  # this is the template/pattern for the keys
+    **_key_template_kwargs,
+)
+abstracts_parquet_codec = Pipe(
+    abstracts_key_template.filt_iter(
+        'str'
+    ),  # filter in only keys that match the pattern
+    abstracts_key_template.key_codec('single'),  # just get a single integer as the key
+    *_parquet_codec_end_pipe,
+)
+
+
+def _kind_router(k, v):
+    if k.startswith('titles'):
+        return titles_parquet_codec(v)
+    elif k.startswith('abstracts'):
+        return abstracts_parquet_codec(v)
+    raise KeyError(f'Invalid key: {k}')
+
+# --------------------------------------------------------------------------------------
+
+sources = wrap_kvs(raw_sources, postget=_kind_router)
+
+arxiv_url_template = "https://arxiv.org/{resource}/{doi}"
+
+ArxivResource = Literal['abs', 'pdf', 'format', 'src', 'cits']
+
+resource_descriptions = {
+    "abs": "Main page of article. Contains links to all other relevant information.",
+    "pdf": "Direct link to article pdf",
+    "format": "Page giving access to other formats",
+    "src": "Access to the original source files submitted by the authors.",
+    "cits": "Tracks citations of the article across various platforms and databases.",
+}
+
+
+def arxiv_url(doi: str, resource: ArxivResource = 'abs') -> str:
+    """Return the url for the given DOI and resource."""
+    return arxiv_url_template.format(doi=doi, resource=resource)
---
+++ imbed/util.py
@@ -0,0 +1,142 @@
+"""Utils for saying package."""
+
+import os
+import importlib.resources
+from functools import partial, cached_property
+from typing import Mapping, Callable, Optional, TypeVar, KT, Iterable, Any
+from config2py import get_app_data_folder
+from graze import (
+    graze as _graze,
+    Graze as _Graze,
+    GrazeReturningFilepaths as _GrazeReturningFilepaths,
+)
+
+import re
+import numpy as np
+
+MappingFactory = Callable[..., Mapping]
+
+package_name = 'imbed'
+
+DFLT_DATA_DIR = get_app_data_folder(package_name, ensure_exists=True)
+GRAZE_DATA_DIR = get_app_data_folder(
+    os.path.join(package_name, 'graze'), ensure_exists=True
+)
+graze_kwargs = dict(
+    rootdir=GRAZE_DATA_DIR,
+    preget=_graze.preget_print_downloading_message_with_size,
+)
+graze = partial(_graze, **graze_kwargs)
+grazed_path = partial(graze, return_filepaths=True)
+Graze = partial(_Graze, **graze_kwargs)
+GrazeReturningFilepaths = partial(_GrazeReturningFilepaths, **graze_kwargs)
+
+
+non_alphanumeric_re = re.compile(r'\W+')
+
+
+def lower_alphanumeric(text):
+    return non_alphanumeric_re.sub(' ', text).strip().lower()
+
+
+def hash_text(text):
+    """Return a hash of the text, ignoring punctuation and capitalization.
+
+    >>> (assert hash_text('Hello, world!')
+    ...     ==  hash_text('hello world')
+    ...     == '5eb63bbbe01eeed093cb22bb8f5acdc3'
+    ... )
+
+    """
+    from hashlib import md5
+
+    normalized_text = lower_alphanumeric(text)
+    return md5(normalized_text.encode()).hexdigest()
+
+
+def lenient_bytes_decoder(bytes_: bytes):
+    if isinstance(bytes_, bytes):
+        return bytes_.decode('utf-8', 'replace')
+    return bytes_
+
+
+def clog(condition, *args, log_func=print, **kwargs):
+    """Conditional log
+
+    >>> clog(False, "logging this")
+    >>> clog(True, "logging this")
+    logging this
+
+    """
+    if not args and not kwargs:
+        import functools
+
+        return functools.partial(clog, condition, log_func=log_func)
+    if condition:
+        log_func(*args, **kwargs)
+
+
+# mdat utils
+
+
+def is_submodule_path(path):
+    path = str(path)
+    return path.endswith('.py')
+
+
+def module_name(path):
+    name, ext = os.path.splitext(os.path.basename(path))
+    return name
+
+
+def submodules_of(pkg, include_init=True):
+    f = importlib.resources.files(pkg)
+    g = map(module_name, filter(is_submodule_path, f.iterdir()))
+    if include_init:
+        return g
+    else:
+        return filter(lambda name: name != '__init__', g)
+
+
+EmbeddingKey = TypeVar('EmbeddingKey')
+Metadata = Any
+MetaFunc = Callable[[EmbeddingKey], Metadata]
+
+
+class Embeddings:
+    def __init__(
+        self,
+        embeddings,
+        keys: Optional[Iterable[EmbeddingKey]] = None,
+        *,
+        meta: Optional[MetaFunc] = None,
+        max_query_hits: int = 5,
+    ):
+        self.embeddings = np.array(embeddings)
+        if keys is None:
+            keys = range(len(embeddings))
+        self.keys = keys
+        self._meta = meta
+
+    @classmethod
+    def from_mapping(cls, mapping: Mapping[EmbeddingKey, object], *, meta):
+        return cls(mapping.values(), mapping.keys())
+
+    @classmethod
+    def from_dataframe(cls, df, *, meta, embedding_col='embedding', key_col=None):
+        if key_col is None:
+            return cls(df[embedding_col], meta=meta)
+        else:
+            return cls(df[embedding_col], keys=df[key_col], meta=meta)
+
+    def search(self, query_embedding, n=None):
+        """Return the n closest embeddings to the query embedding."""
+        from sklearn.metrics.pairwise import cosine_similarity
+
+        n = n or self.max_query_hits
+        similarities = cosine_similarity(
+            query_embedding.reshape(1, -1), self.embeddings
+        )
+        return sorted(
+            zip(self.keys, similarities[0]), key=lambda x: x[1], reverse=True
+        )[:n]
